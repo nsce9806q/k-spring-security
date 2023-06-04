8730fc4932b8ac2ebe33a1ec52e65b2ba7ea5ccb
ChangEn Yea <nsce9806q@gmail.com>
2023-06-04 10:47:32 +0900

    fix(util): JWT 디펜던시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("com.auth0:java-jwt:4.4.0")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
