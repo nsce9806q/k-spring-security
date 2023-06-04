86609ed90d25e9cdc1084a3c9dc83d478ebc7360
ChangEn Yea <nsce9806q@gmail.com>
2023-06-04 11:38:52 +0900

    fix(core): KauthConfigurer에서 미사용 필드 제거

--- src/main/java/org/swmaestro/kauth/core/KauthConfigurer.java
+++ src/main/java/org/swmaestro/kauth/core/KauthConfigurer.java
@@ -1,12 +1,10 @@
 package org.swmaestro.kauth.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.Filter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Kauth 설정 시작 점, 인증/인가 전략 선택
@@ -15,10 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public abstract class KauthConfigurer {
 
     protected HttpSecurity http;
-
-    protected UserDetailsService userDetailsService;
-
-    protected ObjectMapper objectMapper;
 
     protected AuthenticationManager authenticationManager;
 
