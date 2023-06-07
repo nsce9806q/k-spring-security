bebbc236ca892d893a5a49545d987550670a16e5
ChangEn Yea <nsce9806q@gmail.com>
2023-06-07 20:18:25 +0900

    시큐리티 필터 체인 설정 예제 업데이트

--- src/main/java/org/swmaestro/kauth/test/SecurityConfiguration.java
+++ src/main/java/org/swmaestro/kauth/test/SecurityConfiguration.java
@@ -2,10 +2,10 @@ package org.swmaestro.kauth.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.swmaestro.kauth.core.JwtConfigurer;
+import org.swmaestro.kauth.core.jwt.JwtFilterChainBuilder;
 
 /**
  * TODO Release전에 지워야 함
@@ -16,12 +16,15 @@ import org.swmaestro.kauth.core.JwtConfigurer;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        JwtConfigurer.init(http).complete();
-
-        return http.build();
+    public SecurityFilterChain filterChain(JwtFilterChainBuilder builder) throws Exception {
+        return builder.init()
+            .UsernamePassword("/login")
+            .build();
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
