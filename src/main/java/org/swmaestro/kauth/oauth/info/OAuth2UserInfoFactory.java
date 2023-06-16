8dd3b2de019f72af2ab6ed9051545f58e47483fa
최승표 <cspyo98@naver.com>
2023-06-16 18:13:02 +0900

    Feat: OAuth2.0 kakao, naver, google 기능을 위한 OAuth2User, service 구현체 생성

---
+++ src/main/java/org/swmaestro/kauth/oauth/info/OAuth2UserInfoFactory.java
@@ -0,0 +1,23 @@
+package org.swmaestro.kauth.oauth.info;
+
+import org.swmaestro.kauth.oauth.info.provider.ProviderType;
+import org.swmaestro.kauth.oauth.info.provider.GoogleOAuth2UserInfo;
+import org.swmaestro.kauth.oauth.info.provider.KakaoOAuth2UserInfo;
+import org.swmaestro.kauth.oauth.info.provider.NaverOAuth2UserInfo;
+
+import java.util.Map;
+
+/**
+ * OAuth Provider 별로 사용자 정보를 반환하는 Factory Class
+ * @author pyo
+ */
+public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+        switch (providerType) {
+            case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
+            case NAVER: return new NaverOAuth2UserInfo(attributes);
+            case KAKAO: return new KakaoOAuth2UserInfo(attributes);
+            default: throw new IllegalArgumentException("Unsupported Provider Type");
+        }
+    }
+}
---
+++ src/main/java/org/swmaestro/kauth/oauth/info/provider/ProviderType.java
@@ -0,0 +1,9 @@
+package org.swmaestro.kauth.oauth.info.provider;
+
+/**
+ * 제공하는 OAuth2 서비스 Provider Enum
+ * @author pyo
+ */
+public enum ProviderType {
+    GOOGLE, NAVER, KAKAO
+}
