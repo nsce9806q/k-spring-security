38fab65d780246b66649a13faea4279c71311df6
최승표 <cspyo98@naver.com>
2023-06-16 23:09:16 +0900

    Fix: OAuth2UserInfoFactory를 스프링 빈 등록하도록 변경

--- src/main/java/org/swmaestro/kauth/oauth/info/OAuth2UserInfoFactory.java
+++ src/main/java/org/swmaestro/kauth/oauth/info/OAuth2UserInfoFactory.java
@@ -1,5 +1,6 @@
 package org.swmaestro.kauth.oauth.info;
 
+import org.springframework.stereotype.Component;
 import org.swmaestro.kauth.oauth.info.provider.ProviderType;
 import org.swmaestro.kauth.oauth.info.provider.GoogleOAuth2UserInfo;
 import org.swmaestro.kauth.oauth.info.provider.KakaoOAuth2UserInfo;
@@ -11,8 +12,9 @@ import java.util.Map;
  * OAuth Provider 별로 사용자 정보를 반환하는 Factory Class
  * @author pyo
  */
+@Component
 public class OAuth2UserInfoFactory {
-    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+    public OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
         switch (providerType) {
             case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
             case NAVER: return new NaverOAuth2UserInfo(attributes);
--- src/main/java/org/swmaestro/kauth/oauth/service/KOAuth2UserService.java
+++ src/main/java/org/swmaestro/kauth/oauth/service/KOAuth2UserService.java
@@ -1,5 +1,6 @@
 package org.swmaestro.kauth.oauth.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,14 +14,18 @@ import org.swmaestro.kauth.oauth.info.OAuth2UserInfoFactory;
  * Provider 로부터 받은 OAuth2 유저(User) 정보를 기존 계정(Member)과 연계하여 처리(회원가입, 정보 갱신 등)하는 서비스
  * 사용자는 이 클래스를 상속하는 Custom 서비스 클래스를 만들고
  * processMemberByOAuth2UserInfo 메소드에 기존 계정(사용자가 정의한 멤버 도메인 모델)과 연계해서 처리하는 기능을 구현한다.
- * <M> : 유저(멤버) 도메인 모델
  * @author pyo
  */
+@RequiredArgsConstructor
 public abstract class KOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final OAuth2UserInfoFactory oAuth2UserInfoFactory;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User user = super.loadUser(userRequest);
         OAuth2UserInfo oAuth2UserInfo = makeOAuth2UserInfoByProvider(userRequest, user);
+
         return processMemberByOAuth2UserInfo(oAuth2UserInfo);
     }
 
@@ -35,11 +40,12 @@ public abstract class KOAuth2UserService extends DefaultOAuth2UserService {
         ProviderType providerType = ProviderType.valueOf(
                 userRequest.getClientRegistration().getRegistrationId().toUpperCase());
 
-        return OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, user.getAttributes());
+        return oAuth2UserInfoFactory.getOAuth2UserInfo(providerType, user.getAttributes());
     }
 
     /**
      * OAuth2UserInfo 에서 얻은 정보를 바탕으로 기존 계정(Member)과 연계하여 처리(회원가입, 정보 갱신 등)하는 메소드
+     * return 예시: return new KauthUserPrincipal(member, oAuth2UserInfo.getAttributes());
      * @param oAuth2UserInfo
      * @return (KauthUserPrincipal) 사용자가 정의한 멤버 모메인 모델과 OAuth2UserInfo의 attributes을 주입한 객체 반환
      */
