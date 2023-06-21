2243378481ec3b859f2c1242ecb31e76c7a8a611
ChangEn Yea <nsce9806q@gmail.com>
2023-06-21 09:15:44 +0900

    Fix: UserDetailsService.loadUserByUsername 메소드가 null 반환시 UsernameNotFoundException이 발생하도록 수정

--- src/main/java/org/swmaestro/kauth/authentication/UsernamePasswordAuthenticationManager.java
+++ src/main/java/org/swmaestro/kauth/authentication/UsernamePasswordAuthenticationManager.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.swmaestro.kauth.core.user.PostAuthenticationService;
@@ -56,6 +57,10 @@ public class UsernamePasswordAuthenticationManager implements AuthenticationMana
 
 		AuthenticationProvider auth = (AuthenticationProvider)authentication;
 		UserDetails user = userDetailsService.loadUserByUsername((String)auth.getPrincipal());
+
+		if (user == null) {
+			throw new UsernameNotFoundException("userDetailsService.loadUserByUsername returns null");
+		}
 
 		// AccountStatusException 체크
 		userDetailsChecker.check(user);
