8d62c505a3e51999967e73eb358e4a6fc0e07e25
ChangEn Yea <nsce9806q@gmail.com>
2023-06-21 09:13:18 +0900

    Docs: JavaDoc 추가

--- src/main/java/org/swmaestro/kauth/core/user/KauthUserDetailsService.java
+++ src/main/java/org/swmaestro/kauth/core/user/KauthUserDetailsService.java
@@ -2,7 +2,9 @@ package org.swmaestro.kauth.core.user;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+/**
+ * Kauth로 {@link org.springframework.security.web.SecurityFilterChain} 구성시 구현이 필요한 인터페이스
+ * @author ChangEn Yea
+ */
 public interface KauthUserDetailsService extends UserDetailsService, PostAuthenticationService {
-
-//	TODO 마지막 로그인 시간, 비밀번호 오류 횟수, 계정 잠금 등 설정하는 메소드
 }
--- src/main/java/org/swmaestro/kauth/core/user/RefreshTokenService.java
+++ src/main/java/org/swmaestro/kauth/core/user/RefreshTokenService.java
@@ -5,7 +5,18 @@ package org.swmaestro.kauth.core.user;
  * @author ChangEn Yea
  */
 public interface RefreshTokenService {
+
+	/**
+	 * username으로 RefreshToken를 가져온다.
+	 * @param username
+	 * @return RefreshToken
+	 */
 	default String getRefreshToken(String username) { return null; }
 
+	/**
+	 * refreshToken을 서버에 저장한다.
+	 * @param refreshToken
+	 * @param username
+	 */
 	default void setRefreshToken(String refreshToken, String username) {}
 }
